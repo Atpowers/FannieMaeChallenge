44216b14aaad9dff7449f556f0df4604d0fa7458
Atpowers <65352937+Atpowers@users.noreply.github.com>
2020-05-18 12:33:54 -0400

    Add files via upload
    Second commit changes to HTML

--- FannieMaeChallengeATP/JsonParse.js
+++ FannieMaeChallengeATP/JsonParse.js
@@ -1,8 +1,15 @@
 function JsonParse(){
-var file = new XMLHttpRequest();
-File.overrideMimeType("application/json");
-file.open("GET",file);
-file.responseType = 'text';
-var mydata = Json.parse(file.responseText);
-console.log(mydata);
+ var filereader = new Filereader();
+ reader.onload = (function(reader){
+   return function()
+   {
+     var json = reader.results;
+     var mydata = JSON.parse(json);
+     console.log(mydata);
+   }
+ })(reader);
+
+
 }
+
+//JsonParse();
